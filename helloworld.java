e4642f70c56046d1f492b400b750b221a2c94e22
chrisshibu <135575911+chrisshibu@users.noreply.github.com>
2024-11-30 17:27:45 +0530

    Modified

--- helloworld.java
+++ helloworld.java
@@ -2,7 +2,7 @@ class QuickStart {
 
     public static void main(String[] args){
 
-        // this prints hello world to the console
+        // this prints hello world to the 
         System.out.println("hello world!");
 
     }
